fdb7b220f115e2d1aea189e38dd5ea4f367f26b8
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-03-31 00:44:38 +0200

    Design the Quetions Page
    Here I added a design for the questions and the answers

--- app/src/main/java/com/haz7/quizlearn/QuizActivity.kt
+++ app/src/main/java/com/haz7/quizlearn/QuizActivity.kt
@@ -2,10 +2,37 @@ package com.haz7.quizlearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haz7.quizlearn.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
+
+    lateinit var quizBinding: ActivityQuizBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
+        quizBinding = ActivityQuizBinding.inflate(layoutInflater)
+        val view = quizBinding.root
+        setContentView(view)
+
+        quizBinding.buttonNext.setOnClickListener{
+
+        }
+
+        quizBinding.buttonFinish.setOnClickListener {
+
+        }
+
+        quizBinding.textViewA.setOnClickListener{
+
+        }
+        quizBinding.textViewB.setOnClickListener{
+
+        }
+        quizBinding.textViewC.setOnClickListener{
+
+        }
+        quizBinding.textViewD.setOnClickListener{
+
+        }
     }
 }
