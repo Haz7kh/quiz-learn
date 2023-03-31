93e63dd00c6eaa79bdde9d9070815db089fab18e
khaled Almelhem <90411108+Haz7kh@users.noreply.github.com>
2023-03-31 02:01:26 +0200

    fix the database
    add some questions to the firebase and show them in the app

--- app/src/main/java/com/haz7/quizlearn/QuizActivity.kt
+++ app/src/main/java/com/haz7/quizlearn/QuizActivity.kt
@@ -2,11 +2,29 @@ package com.haz7.quizlearn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.haz7.quizlearn.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
 
     lateinit var quizBinding: ActivityQuizBinding
+
+    private val database = FirebaseDatabase.getInstance()
+    private val databaseReference = database.reference.child("questions")
+
+    var question = ""
+    var answerA = ""
+    var answerB = ""
+    var answerC = ""
+    var answerD = ""
+    var correctAnswer = ""
+    var questionCount = 0
+    var questionNumber = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +32,10 @@ class QuizActivity : AppCompatActivity() {
         val view = quizBinding.root
         setContentView(view)
 
-        quizBinding.buttonNext.setOnClickListener{
+        gameLogic()
 
+        quizBinding.buttonNext.setOnClickListener{
+          gameLogic()
         }
 
         quizBinding.buttonFinish.setOnClickListener {
@@ -34,5 +54,42 @@ class QuizActivity : AppCompatActivity() {
         quizBinding.textViewD.setOnClickListener{
 
         }
+    }
+    private fun gameLogic() {
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                questionCount = snapshot.childrenCount.toInt()
+                if(questionNumber <= questionCount){
+                    question = snapshot.child(questionNumber.toString()).child("q").value.toString()
+                    answerA = snapshot.child(questionNumber.toString()).child("a").value.toString()
+                    answerB = snapshot.child(questionNumber.toString()).child("b").value.toString()
+                    answerC = snapshot.child(questionNumber.toString()).child("c").value.toString()
+                    answerD = snapshot.child(questionNumber.toString()).child("d").value.toString()
+                    correctAnswer = snapshot.child(questionNumber.toString()).child("answer").value.toString()
+
+                    quizBinding.textViewQuestion.text = question
+                    quizBinding.textViewA.text = answerA
+                    quizBinding.textViewB.text = answerB
+                    quizBinding.textViewC.text = answerC
+                    quizBinding.textViewD.text = answerD
+
+                    quizBinding.progressBarQuiz.visibility = View.INVISIBLE
+                    quizBinding.linearLayoutInfo.visibility = View.VISIBLE
+                    quizBinding.linearLayoutQuestion.visibility = View.VISIBLE
+                    quizBinding.linearLayoutBtns.visibility = View.VISIBLE
+                } else {
+                    Toast.makeText(applicationContext,"Your answered all the questions",Toast.LENGTH_SHORT).show()
+                }
+                questionNumber++
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext,error.message,Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
